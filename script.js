bd9a5c044451ae3292e773477e83de1b2fa95b6c
Suraj Kumar <sahsuraj2001@gmail.com>
2023-04-15 16:36:33 +0530

    validate Form

--- script.js
+++ script.js
@@ -2,86 +2,307 @@ const barsEl = document.querySelector(".bars");
 const linkItemEl = document.querySelector(".link-item");
 const formEl = document.querySelector("form");
 const previewEl = document.querySelector(".preview");
-let fName = document.getElementById("fname");
-let mName = document.getElementById("mname");
-let lName = document.getElementById("lname");
-let momName = document.getElementById("mothername");
-let dadName = document.getElementById("fathername");
-let mobNo = document.getElementById("mob");
-let gender = document.getElementsByName("gender");
-let dayEl = document.getElementById("day");
-let monthEl = document.getElementById("month");
-let yearEl = document.getElementById("year");
-let dob = document.getElementById("dob");
-let email = document.getElementById("email");
-let pwd = document.getElementById("password");
-let conpwd = document.getElementById("conpassword");
-let user = document.getElementById("user");
-let roll = document.getElementById("roll");
-let course = document.getElementById("course");
-let clg = document.getElementById("clgname");
-let universityEl = document.getElementById("university");
-let cityEl = document.getElementById("city");
-let state = document.getElementById("state");
-let pin = document.getElementById("pin");
-let photo = document.getElementById("photo");
-let tc = document.getElementById("tc");
+const fName = document.getElementById("fname");
+const mName = document.getElementById("mname");
+const lName = document.getElementById("lname");
+const momName = document.getElementById("mothername"); 
+const dadName = document.getElementById("fathername");
+const mobNo = document.getElementById("mob");
+// document.querySelector('input[name="gender"]:checked')
+
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const email = document.getElementById("email_id");
+const pwd = document.getElementById("password");
+const conpwd = document.getElementById("conpassword");
+const user = document.getElementById("user");
+const roll = document.getElementById("roll");
+const course = document.getElementById("course");
+const clg = document.getElementById("clgname");
+const university = document.getElementById("university");
+const city = document.getElementById("city");
+const state = document.getElementById("state");
+const pin = document.getElementById("pin");
+const photo = document.getElementById("photo");
 
 
-console.log(gender)
+
 // for mobile device Control 
-
 barsEl.addEventListener("click", () => {
     linkItemEl.classList.toggle("block")
 })
 
-// form bydefault submit Stop 
-formEl.addEventListener("submit", (event) => {
-    event.preventDefault();
-});
 
-// preview the form 
-previewEl.addEventListener("click", () => {
-    // if(fName){
-    //     alert("sorry");
-    // }else{
-        document.getElementById("fullName").innerHTML = `${fName.value} ${mName.value} ${lName.value} `;
-    // }
-    document.getElementById("motherName").innerHTML = momName.value;
-    document.getElementById("fatherName").innerHTML = dadName.value;
-    document.getElementById("phone").innerHTML = mobNo.value;
+// Set Error Message 
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
 
-    // gender Checking       
-    for(i = 0; i < gender.length; i++) {
-        if(gender[i].checked)
-        document.getElementById("genderEl").innerHTML = gender[i].value;
+    errorDisplay.innerText = message;
+}
+
+// Set Success Message 
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+};
+
+photo.onchange = evt => {
+    const [file] = photo.files
+    if (file) {
+      blah.src = URL.createObjectURL(file)
     }
-
-    document.getElementById("dobEl").innerHTML = `${dayEl.value} ${monthEl.value} ${yearEl.value}`;
+  }
+// form bydefault submit Stop 
+// formEl.addEventListener("submit", (event) => {
+//     event.preventDefault();
 
     
-    document.getElementById("emailId").innerHTML = email.value;
+// });
 
-    // password Verifying
-    if(pwd.value === conpwd.value && pwd.value != ""){
-        document.getElementById("passwordEl").innerHTML = pwd.value;
-    }else{
-        alert("Please Enter Same Password");
+
+// preview the form 
+formEl.addEventListener("submit", (event) => {
+// console.log(gender.value) 
+event.preventDefault();
+
+
+    const firstName = fName.value;
+    const lastName = lName.value;
+    const momNameEl = momName.value;
+    const dadNameEl = dadName.value;
+    const mobNoEl = mobNo.value;
+    // const genderEl = document.forms['myForm']['gender'];
+    const gender = document.getElementsByName('gender');
+    let monthValue = month.value;
+    const dayEl = day.value;
+    const yearEl = year.value;
+    const dob = `${dayEl} ${monthValue} ${yearEl}`
+    const emailEl = email.value;
+    const pwdEl = pwd.value;
+    const conpwdEl = conpwd.value;
+    const userEl = user.value;
+    const rollEl = roll.value;
+    const courseEl = course.value;
+    const clgEl = clg.value;
+    const universityEl = university.value;
+    const cityEl = city.value;
+    const stateEl = state.value;
+    const pinCode = pin.value; 
+    const photoEl = photo.value;   
+    const tc = document.getElementById("tc");
+    
+    // if(dayEl === ''){
+    //     // setError(day, "day")
+    //     console.log('error')
+    // }else{
+    //     // setSuccess(day)
+    //     console.log(dob)
+    // }
+
+    const isPhone = /^[6789][0-9]{9}$/;
+    const isEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    
+    
+    if(firstName === '') {
+        fName.classList.add('is-invalid');
+        setError(fName, 'First Name is required');
+    } else {
+        setSuccess(fName);
+        fName.classList.remove('is-invalid');
+        fName.classList.add('is-valid');
     }
 
-    document.getElementById("conpassword").innerHTML = conpwd.value;
-    document.getElementById("userName").innerHTML = user.value;
-    document.getElementById("rollNo").innerHTML = roll.value;
-    document.getElementById("courseName").innerHTML = course.value;
-    document.getElementById("clgName").innerHTML = clg.value;
-    document.getElementById("uinversityName").innerHTML = universityEl.value;
-    document.getElementById("cityName").innerHTML = cityEl.value;
-    document.getElementById("stateName").innerHTML = state.value;
-    document.getElementById("pinCode").innerHTML = pin.value;
-    // let userPhoto = photo.value;
-    // let t_c = tc.value;
+    if(lastName === '') {
+        lName.classList.add('is-invalid');
+        setError(lName, 'Last Name is required');
+    } else {
+        setSuccess(lName)
+        lName.classList.remove('is-invalid');
+        lName.classList.add('is-valid');
+    }
 
-    console.log(universityEl.value)
+    if(momNameEl === '') {
+        momName.classList.add('is-invalid');
+        setError(momName, `Mother's Name is required`);
+    } else {
+        setSuccess(momName);
+        momName.classList.remove('is-invalid');
+        momName.classList.add('is-valid');
+    }
+
+    if(dadNameEl === '') {
+        dadName.classList.add('is-invalid');
+        setError(dadName, `Father's Name is required`);
+    } else {
+        setSuccess(dadName);
+        dadName.classList.remove('is-invalid');
+        dadName.classList.add('is-valid');
+    }
+
+    if(!isPhone.test(mobNoEl)) {
+        mobNo.classList.add('is-invalid');
+        setError(mobNo, 'Mobile No. is Invalid');
+    } else {
+        setSuccess(mobNo);
+        mobNo.classList.remove('is-invalid');
+        mobNo.classList.add('is-valid');
+    }
+
+    for(i = 0; i < gender.length; i++) {
+        if(gender[i].checked){
+            console.log(gender[i].value)
+        }
+    }
+  
+    // if(dayEl === '') {
+    //     day.classList.add('is-invalid');
+    //     setError(day, `Day is required`);
+    // } else {
+    //     console.log(dayEl);
+    //     day.classList.remove('is-invalid');
+    //     day.classList.add('is-valid');
+    // }
+
+    if(!isEmail.test(emailEl)) {
+        email.classList.add('is-invalid');
+        setError(email, 'Email is Invalid');
+    } else {
+        setSuccess(email);
+        email.classList.remove('is-invalid');
+        email.classList.add('is-valid');
+    }
+
+    if(pwdEl === '') {
+        pwd.classList.add('is-invalid');
+        setError(pwd, 'Password is required');
+    } else {
+        setSuccess(pwd);
+        pwd.classList.remove('is-invalid');
+        pwd.classList.add('is-valid');
+    }
+
+    if(conpwdEl === '') {
+        conpwd.classList.add('is-invalid');
+        setError(conpwd, 'Confirm Password is required');
+    } else {
+        setSuccess(conpwd);
+        conpwd.classList.remove('is-invalid');
+        
+    }
+
+    if(pwdEl !== conpwdEl){
+        conpwd.classList.add('is-invalid');
+        setError(conpwd, 'Confirm Password is not match');
+    }else{
+        setSuccess(conpwd);
+        conpwd.classList.add('is-valid');
+    }
+
+    if(userEl === '') {
+        user.classList.add('is-invalid');
+        setError(user, 'User Name is required');
+    } else {
+        setSuccess(user);
+        user.classList.remove('is-invalid');
+        user.classList.add('is-valid');
+    }
+
+    if(rollEl === '') {
+        roll.classList.add('is-invalid');
+        setError(roll, 'Roll No. is required');
+    } else {
+        setSuccess(roll);
+        roll.classList.remove('is-invalid');
+        roll.classList.add('is-valid');
+    }
+
+    if(courseEl === '') {
+        course.classList.add('is-invalid');
+        setError(course, 'Course is required');
+    } else {
+        setSuccess(course);
+        course.classList.remove('is-invalid');
+        course.classList.add('is-valid');
+    }
+
+    if(clgEl === '') {
+        clg.classList.add('is-invalid');
+        setError(clg, 'College is required');
+    } else {
+        setSuccess(clg);
+        clg.classList.remove('is-invalid');
+        clg.classList.add('is-valid');
+    }
+
+    if(universityEl === '') {
+        university.classList.add('is-invalid');
+        setError(university, 'University is required');
+    } else {
+        setSuccess(university);
+        university.classList.remove('is-invalid');
+        university.classList.add('is-valid');
+    }
+
+    if(cityEl === '') {
+        city.classList.add('is-invalid');
+        setError(city, 'City is required');
+    } else {
+        setSuccess(city);
+        city.classList.remove('is-invalid');
+        city.classList.add('is-valid');
+    }
+
+    if(stateEl === '') {
+        state.classList.add('is-invalid');
+        setError(state, 'State is required');
+    } else {
+        setSuccess(state);
+        state.classList.remove('is-invalid');
+        state.classList.add('is-valid');
+    }
+
+    if(pinCode === '') {
+        pin.classList.add('is-invalid');
+        setError(pin, 'Pin Code is required');
+    } else {
+        setSuccess(pin);
+        pin.classList.remove('is-invalid');
+        pin.classList.add('is-valid');
+    }
+
+    if(photoEl === '') {
+        photo.classList.add('is-invalid');
+        setError(photo, 'Photo is required');
+    } else {
+        setSuccess(photo);
+        photo.classList.remove('is-invalid');
+        photo.classList.add('is-valid');
+    }
+
+    if(!tc.checked) {
+        setError(tc, 'Accept T&C');
+    } else {
+        setSuccess(tc);
+        console.log('checked')
+        
+    }
+   if(firstName, lastName, momNameEl, dadNameEl,conpwdEl,userEl, rollEl, courseEl, clgEl, universityEl, cityEl, stateEl, pinCode, photoEl === ''){
+    return false
+   }else if (!isPhone.test(mobNoEl), !isEmail.test(emailEl), !tc.checked){
+    return false
+   } else if(pwdEl !== conpwdEl){
+    return false;
+   } else {
+    alert('Form Validate SuccessFully.......🔥🔥🔥🔥🔥🔥🔥🔥')
+   }
 
 
 })
+
+
+
+
